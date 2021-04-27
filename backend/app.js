940ee18bc7ccc5e0b7591362548195bef8d4b290
oagnouba <oagnouba@gmail.com>
2021-04-27 10:48:45 +0200

    creation of the app

---
+++ backend/app.js
@@ -0,0 +1,7 @@
+const express = require("express"); //créer l'instance express
+
+const app = express(); //créer l'application express
+
+
+
+module.exports = app;//exporter 'app' pour y avoir accès depuis les autres fichiers
